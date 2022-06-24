71e4ad57d5527ff94078749ef98f4bb05a502954
Suraj Mishra <mishrasur7@gmail.com>
2022-06-24 14:55:25 +0300

    white space removed

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,6 @@ function App() {
           <Route path="/profile" element={<Profile />} />
           <Route path="/cart" element={<Cart />} />
         </Routes>
-
       </Router>
     </div>
   );
