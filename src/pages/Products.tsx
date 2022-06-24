73f4b8a7f95177bf18572c26c87a743faf92ba54
Suraj Mishra <mishrasur7@gmail.com>
2022-06-24 22:43:58 +0300

    styles tested

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { Card, CardContent, CardMedia, CardHeader, Typography, Button } from '@mui/material'
+
+import { useAppSelector } from '../redux/hooks'
+import '../styles/pages/products.scss'
 
 function Products() {
+  const products = useAppSelector(state => state.productReducer)
   return (
-    <div>Products</div>
+    // this is just testing
+    <div className='products'>
+      {products.map(product => (
+        <Card key={product.id} className='card'>
+          <CardHeader title={product.title}/>
+          <CardMedia
+          component='img'
+          image={product.category.image}
+          />
+          <CardContent>
+            <Typography>Description: {product.description}</Typography>
+            <Typography>Price: {product.price} $</Typography>
+            <Button>Add to cart</Button>
+          </CardContent>
+        </Card>
+      ))}
+    </div>
   )
 }
+
 
 export default Products
