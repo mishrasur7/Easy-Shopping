5891e99c0e2031e2691911dd1b6551b4ebb91408
Suraj Mishra <mishrasur7@gmail.com>
2022-07-07 19:47:48 +0300

    cartReducer added

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import { useAppSelector } from '../redux/hooks/appHooks'
+import { useAppSelector } from "../redux/hooks/appHooks";
 
 function Profile() {
-  const user = useAppSelector((state) => state.userReducer.user)
+  const user = useAppSelector((state) => state.userReducer.user);
   return (
     <div>
-      {user &&(
+      {user ? (
         <div>
-        <p>First name: {user.firstName}</p>
-        <p>Last name: {user.lastName}</p>
-        <p>Email: {user.email}</p>
-        <p>Google Id: {user.id}</p>
+          <p>First name: {user.firstName}</p>
+          <p>Last name: {user.lastName}</p>
+          <p>Email: {user.email}</p>
+          <p>Google Id: {user.id}</p>
         </div>
+      ) : (
+        <div>Please log in first</div>
       )}
     </div>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
